bc775b65a2e501713db2d9445cba196bf14f062f
maksym.ivanov <Maksym.Ivanov-contractor@RobertWalters.com>
2024-05-14 21:56:59 +0300

    add a test for context class

--- src/main/java/home/javaqa/mi/MyMath.java
+++ src/main/java/home/javaqa/mi/MyMath.java
@@ -5,8 +5,4 @@ public class MyMath {
     public static int sum(int a, int b) {
         return a + b;
     }
-
-    public static void main(String[] args) {
-        System.out.println("Hi");
-    }
 }
