5dc9d98f7f76519b45b4ed96ca95589312fa85ca
maksym.ivanov <Maksym.Ivanov-contractor@RobertWalters.com>
2024-05-14 21:01:00 +0300

    add a first test for sum method

--- src/main/java/home/javaqa/mi/MyMath.java
+++ src/main/java/home/javaqa/mi/MyMath.java
@@ -5,4 +5,8 @@ public class MyMath {
     public static int sum(int a, int b) {
         return a + b;
     }
+
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
 }
